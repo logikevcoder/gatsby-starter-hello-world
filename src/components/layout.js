6bf6ae72276ebfd35b280ac847f5f0e99e4b57b8
Kevin Napier <kevin.napier@rotary.org>
2020-02-28 14:23:13 -0600

    added functionality for styled-components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Navbar from "../components/Navbar"
+import { Button } from "../components/button"
 
 const layout = ({ children }) => {
   return (
     <>
       <Navbar />
+      <Button>Styled Button</Button>
       <main>{children}</main>
     </>
   )
